c9a8f1a5098094a800b4a68a241e96544e36159f
LorentzaZweena <ariva02zweena@gmail.com>
2024-10-15 15:28:02 +0700

    Latihan CRUD | MVC

--- app/Http/Controllers/smkController.php
+++ app/Http/Controllers/smkController.php
@@ -9,8 +9,15 @@ use Illuminate\View\View;
 class smkController extends Controller
 {
     public function index() : View {
-        //ambil data dari db
+        //latest : ambil_data dari db dan menurunkan desc
         $datasmk = smk::latest()->paginate(10);
         return view('test', compact('datasmk'));
+    }
+
+    public function show(string $id) : View{
+        //findorfail : ambil data berdasarkan id
+        $data_satuan = smk::findOrFail($id);
+        //compact untuk mengirim data ke view
+        return view('show2', compact('data_satuan'));
     }
 }
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -26,12 +26,12 @@
                         </form>
                     </div>
                     <div class="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
-                        <button type="button" class="flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                        <a href="/create"><button type="button" class="flex text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                             {{-- <svg class="h-3.5 w-3.5 mr-2" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                 <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
                             </svg> --}}
                             Tambah jurusan
-                        </button>
+                        </button></a>
                         <div class="flex items-center space-x-3 w-full md:w-auto">
                             {{-- <button id="actionsDropdownButton" data-dropdown-toggle="actionsDropdown" class="w-full md:w-auto flex items-center justify-center py-2 px-4 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" type="button">
                                 <svg class="-ml-1 mr-1.5 w-5 h-5" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
@@ -58,8 +58,9 @@
                             <tr>
                                 <th scope="col" class="px-4 py-3">No</th>
                                 <th scope="col" class="px-4 py-3 text-center">Jurusan</th>
-                                {{-- <th scope="col" class="px-4 py-3 text-center">Deskripsi</th> --}}
-                                <th scope="col" class="px-4 py-3 ">Aksi</th>
+                                <th scope="col" class="px-4 py-3 text-center">Deskripsi</th>
+                                <th scope="col" class="px-4 py-3 text-center">Foto</th>
+                                <th scope="col" class="px-4 py-3 text-center">Aksi</th>
                                 {{-- <th scope="col" class="px-4 py-3">Brand</th>
                                 <th scope="col" class="px-4 py-3">Description</th>
                                 <th scope="col" class="px-4 py-3">Price</th> --}}
@@ -73,9 +74,10 @@
                             <tr class="border-b dark:border-gray-700">
                                 <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $data->id }}</th>
                                 <th scope="row" class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white text-center">{{ $data->jurusan }}</th>
-                                {{-- <td class="px-4 py-3 text-justify">{{ $data->deskripsi }}</td> --}}
+                                <td class="px-4 py-3 text-justify">{{ $data->deskripsi }}</td>
+                                <td class="px-4 py-3 text-justify">{{ $data->foto }}</td>
                                 <td class="px-4 py-3 flex items-center text-center">
-                                    <button type="button" class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 mr-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Detail</button>
+                                    <a href="{{ route('smk.show', $data->id) }}"><button type="button" class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 mr-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Detail</button></a>
                                     <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
                                     <button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Edit</button>
                                 </td>
